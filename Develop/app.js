8198926da1b3755d27d0228df959f8985434ed7c
luke williams <jluke94williams@gmail.com>
2020-04-06 13:24:50 -0400

    Modified for Heroku

--- Develop/app.js
+++ Develop/app.js
@@ -37,4 +37,8 @@ app.get("/api/notes", function(req, res){
 app.post("/api/notes", function(req, res){
     let createdNote = req.body;
     console.log(createdNote);
-})
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
